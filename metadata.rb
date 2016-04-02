f9b4b3d3a4b3bb7ac24318c3372556641da77866
scherbank <konstantin_scherban@epam.com>
2016-04-02 14:50:57 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures zabbix'
 long_description 'Installs/Configures zabbix'
-version '0.2.0'
+version '0.2.1'
 
 supports 'centos', '~> 6.7'
 
