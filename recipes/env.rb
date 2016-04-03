66f9c83ff25ed7cf99b6bd6e18d6a2ba960bbd33
scherbank <konstantin_scherban@epam.com>
2016-04-03 13:04:26 +0300

    Create env.rb

---
+++ recipes/env.rb
@@ -0,0 +1,29 @@
+   bash "Add a usefull prompt inside .bashrc" do
+       code <<-EOH
+rm -rf /root/ruby
+
+mkdir /root/ruby
+cd /root/ruby
+
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
+
+tar xvzf ruby-1.9.3-p194.tar.gz
+cd ruby-1.9.3-p194
+./configure
+make
+make install
+
+rm -rf /root/rubygem
+mkdir /root/rubygem
+cd /root/rubygem
+
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.24.tgz
+tar xvf rubygems-1.8.24.tgz
+cd rubygems-1.8.24
+ruby setup.rb
+        EOH
+      end
+
+gem_package 'zabcon' do
+  action :install
+end
