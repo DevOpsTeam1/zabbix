19641a32e350c2ce7df993e57ac566bb5c3cbb22
AlehB <Aleh_Belavusau@epam.com>
2016-04-03 17:45:49 +0300

    add gem install and hostgroup add

--- recipes/api_import.rb
+++ recipes/api_import.rb
@@ -1,40 +1,18 @@
-ruby_block 'reload_client_config' do
-  block do
-
-
-zbx = ZabbixApi.connect(
-  :url => 'http://ecsc00104528.epam.com/zabbix/api_jsonrpc.php',
-  :user => 'Aleh',
-  :password => 'zabbix'
-)
-# use basic_auth
-#zbx = ZabbixApi.connect(
-#  :url => 'http://ecsc00104528.epam.com/zabbix/api_jsonrpc.php',
-#  :user => 'Aleh',
-#  :password => 'zabbix',
-#  :http_password => 'foo',
-#  :http_user => 'bar'
-#)
-  end
-  action :run
+chef_gem 'zabbixapi' do
+  version '2.4.7'
 end
 
 ruby_block 'reload_client_config' do
   block do
-zbx.hosts.create_or_update(
-  :host => host.fqdn,
-  :interfaces => [
-    {
-      :type => 17878,
-      :main => 1000,
-      :ip => '10.0.0.1',
-      :dns => 'http://ecsc00104528.epam.com/',
-      :port => 10050,
-      :useip => 0
-    }
-  ],
-  :groups => [ :groupid => zbx.hostgroups.get_id(:name => "NFS") ]
+
+require "zabbixapi"
+
+zbx = ZabbixApi.connect(
+  :url => 'http://ecsc00104528.epam.com/api_jsonrpc.php',
+  :user => 'Admin',
+  :password => 'zabbix'
 )
+zbx.hostgroups.create(:name => "hostgroup1")
   end
   action :run
 end
